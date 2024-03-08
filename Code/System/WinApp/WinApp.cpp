37476082279f2fc6787e38a3035fb84d4da37343
oriharaIssei <k023g0048@g.neec.ac.jp>
2024-03-08 13:13:54 +0900

    window

---
+++ Code/System/WinApp/WinApp.cpp
@@ -0,0 +1,78 @@
+#include "WinApp.h"
+
+LRESULT WinApp::WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
+	//ウィンドウに起こったイベントに対して行う処理
+	switch (msg) {
+	case WM_DESTROY:        // ウィンドウが破棄された
+		PostQuitMessage(0); // OSに対して、アプリの終了を伝える
+		return 0;
+	}
+	return DefWindowProc(hwnd, msg, wparam, lparam); //デフォルトの処理
+}
+
+WinApp::~WinApp() {
+
+}
+
+void WinApp::CreateGameWindow(const wchar_t* title, UINT windowStyle, int32_t clientWidth, int32_t clientHeight) {
+	// COM初期化
+	CoInitializeEx(nullptr, COINIT_MULTITHREADED);
+
+	title_ = title;
+	windowStyle_ = windowStyle;
+
+	//ウィンドウクラスの初期化
+	wndClass_ = std::make_unique<WNDCLASSEX>();
+	wndClass_->cbSize = sizeof(WNDCLASSEX);
+	wndClass_->lpfnWndProc = (WNDPROC)WindowProc;     // ウィンドウプロシージャ
+	wndClass_->lpszClassName = title;      // ウィンドウクラス名
+	wndClass_->hInstance = GetModuleHandle(nullptr);  // ウィンドウハンドル
+	wndClass_->hCursor = LoadCursor(NULL, IDC_ARROW); // カーソル指定
+
+	RegisterClassEx(wndClass_.get());
+
+	// ウィンドウサイズ{ X座標 Y座標 横幅 縦幅 }
+	RECT wrc = { 0, 0, clientWidth, clientHeight };
+	AdjustWindowRect(&wrc, windowStyle_, false);
+
+	// ウィンドウオブジェクトの生成
+	hwnd_ = CreateWindow(
+		wndClass_->lpszClassName, // クラス名
+		title,                   // タイトルバーの文字
+		windowStyle_,            // タイトルバーと境界線があるウィンドウ
+		CW_USEDEFAULT,           // 表示X座標（OSに任せる）
+		CW_USEDEFAULT,           // 表示Y座標（OSに任せる）
+		wrc.right - wrc.left,    // ウィンドウ横幅
+		wrc.bottom - wrc.top,    // ウィンドウ縦幅
+		nullptr,                 // 親ウィンドウハンドル
+		nullptr,                 // メニューハンドル
+		wndClass_->hInstance,     // 呼び出しアプリケーションハンドル
+		nullptr);                // オプション
+	SetWindowLongPtr(hwnd_, GWLP_USERDATA, reinterpret_cast<LONG_PTR>(this));
+
+	// ウィンドウ表示
+	ShowWindow(hwnd_, SW_NORMAL);
+}
+
+void WinApp::TerminateGameWindow() {
+	// ウィンドウクラスを登録解除
+	UnregisterClass(wndClass_->lpszClassName, wndClass_->hInstance);
+
+	// COM 終了
+	CoUninitialize();
+}
+
+bool WinApp::ProcessMessage() {
+	MSG msg{}; // メッセージ
+
+	if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE)) {// メッセージがある？
+		TranslateMessage(&msg); // キー入力メッセージの処理
+		DispatchMessage(&msg);  // ウィンドウプロシージャにメッセージを送る
+	}
+
+	if (msg.message == WM_QUIT) { // 終了メッセージが来たらループを抜ける
+		return true;
+	}
+
+	return false;
+}
