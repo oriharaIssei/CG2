d82e19fab70f05577105959dc805aeec021b05fd
Amaimono <k023g0048@g.neec.ac.jp>
2024-09-03 16:23:04 +0900

    GlobalVariables

--- Code/main.cpp
+++ Code/main.cpp
@@ -3,21 +3,27 @@
 #include <memory>
 
 #include <GameScene.h>
+#include "GlobalVariables.h"
 
 #include "DXDebug.h"
 
 #include <d3d12sdklayers.h>
 
-int WINAPI WinMain(HINSTANCE,HINSTANCE,LPSTR,int) {
+int WINAPI WinMain(HINSTANCE,HINSTANCE,LPSTR,int){
 	std::unique_ptr<DXDebug> debug = std::make_unique<DXDebug>();
 	System *system = System::getInstance();
 	system->Init();
 
+	GlobalVariables *variables = GlobalVariables::getInstance();
+	variables->LoadAllFile();
+
 	std::unique_ptr<GameScene> scene = std::make_unique<GameScene>();
 	scene->Init();
 
-	while(!system->ProcessMessage()) {
+	while(!system->ProcessMessage()){
 		system->BeginFrame();
+
+		variables->Update();
 
 		scene->Update();
 		scene->Draw();
