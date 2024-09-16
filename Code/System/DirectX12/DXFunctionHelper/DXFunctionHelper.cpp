d7bd28ede89c465252cbecd16af6889c2ecb07e4
Amaimono <k023g0048@g.neec.ac.jp>
2024-09-16 10:59:45 +0900

    Add CreateRenderTextureResource()

--- Code/System/DirectX12/DXFunctionHelper/DXFunctionHelper.cpp
+++ Code/System/DirectX12/DXFunctionHelper/DXFunctionHelper.cpp
@@ -2,12 +2,12 @@
 
 #include <cassert>
 
-void DXFunctionHelper::CreateBufferResource(const DXDevice *device,Microsoft::WRL::ComPtr<ID3D12Resource> &resource,size_t sizeInBytes) {
+void DXFunctionHelper::CreateBufferResource(const DXDevice* device,Microsoft::WRL::ComPtr<ID3D12Resource>& resource,size_t sizeInBytes){
 	//頂点リソース用のヒープの設定
-	D3D12_HEAP_PROPERTIES uploadHeapProperties {};
+	D3D12_HEAP_PROPERTIES uploadHeapProperties{};
 	uploadHeapProperties.Type = D3D12_HEAP_TYPE_UPLOAD;//UploadHeapを使う
 	//頂点リソースの設定
-	D3D12_RESOURCE_DESC vertexResourceDesc {};
+	D3D12_RESOURCE_DESC vertexResourceDesc{};
 	//バッファのリソース(テクスチャの場合は別の設定をする)
 	vertexResourceDesc.Dimension = D3D12_RESOURCE_DIMENSION_BUFFER;
 	vertexResourceDesc.Width = sizeInBytes;
@@ -30,10 +30,39 @@ void DXFunctionHelper::CreateBufferResource(const DXDevice *device,Microsoft::WR
 	assert(SUCCEEDED(hr));
 }
 
-void DXFunctionHelper::ClearRenderTarget(const DXCommand *command,const DXSwapChain *swapChain) {
+Microsoft::WRL::ComPtr<ID3D12Resource> DXFunctionHelper::CreateRenderTextureResource(Microsoft::WRL::ComPtr<ID3D12Device> device,uint32_t width,uint32_t height,DXGI_FORMAT format,const Vector4& clearColor){
+	Microsoft::WRL::ComPtr<ID3D12Resource> renderTextureResource;
+	D3D12_RESOURCE_DESC resourceDesc;
+	// RenderTarget として 利用可能に
+	resourceDesc.Flags = D3D12_RESOURCE_FLAG_ALLOW_RENDER_TARGET;
+
+	D3D12_HEAP_PROPERTIES heapProps{};
+	// VRAM 上に 生成
+	heapProps.Type = D3D12_HEAP_TYPE_DEFAULT;
+
+	D3D12_CLEAR_VALUE clearValue;
+	clearValue.Format = format;
+	clearValue.Color[0] = clearColor.x;
+	clearValue.Color[1] = clearColor.y;
+	clearValue.Color[2] = clearColor.z;
+	clearValue.Color[3] = clearColor.w;
+
+	device->CreateCommittedResource(
+		&heapProps,
+		D3D12_HEAP_FLAG_NONE,
+		&resourceDesc,
+		D3D12_RESOURCE_STATE_RENDER_TARGET, // 描画すること を 前提とした テクスチャ なので RenderTarget として 扱う
+		&clearValue,
+		IID_PPV_ARGS(&renderTextureResource)
+	);
+
+	return renderTextureResource;
+}
+
+void DXFunctionHelper::ClearRenderTarget(const DXCommand* command,const DXSwapChain* swapChain){
 	UINT backBufferIndex = swapChain->getCurrentBackBufferIndex();
 
-	DXHeap *heap = DXHeap::getInstance();
+	DXHeap* heap = DXHeap::getInstance();
 
 	D3D12_CPU_DESCRIPTOR_HANDLE dsvHandle = heap->getDsvHeap()->GetCPUDescriptorHandleForHeapStart();
 	D3D12_CPU_DESCRIPTOR_HANDLE backBufferRtvHandle = heap->getRtvCpuHandle(backBufferIndex);
@@ -54,10 +83,11 @@ void DXFunctionHelper::ClearRenderTarget(const DXCommand *command,const DXSwapCh
 	);
 }
 
-void DXFunctionHelper::SetViewportsAndScissor(const DXCommand *dxCommand,const WinApp *window) {
-	ID3D12GraphicsCommandList *commandList = dxCommand->getCommandList();
+
+void DXFunctionHelper::SetViewportsAndScissor(const DXCommand* dxCommand,const WinApp* window){
+	ID3D12GraphicsCommandList* commandList = dxCommand->getCommandList();
 	//ビューポートの設定
-	D3D12_VIEWPORT viewPort {};
+	D3D12_VIEWPORT viewPort{};
 	viewPort.Width = static_cast<float>(window->getWidth());
 	viewPort.Height = static_cast<float>(window->getHeight());
 	viewPort.TopLeftX = 0;
@@ -67,7 +97,7 @@ void DXFunctionHelper::SetViewportsAndScissor(const DXCommand *dxCommand,const W
 
 	commandList->RSSetViewports(1,&viewPort);
 
-	D3D12_RECT scissorRect {};
+	D3D12_RECT scissorRect{};
 	scissorRect.left = 0;
 	scissorRect.right = window->getWidth();
 	scissorRect.top = 0;
@@ -76,19 +106,19 @@ void DXFunctionHelper::SetViewportsAndScissor(const DXCommand *dxCommand,const W
 	commandList->RSSetScissorRects(1,&scissorRect);
 }
 
-void DXFunctionHelper::SetRenderTargets(const DXCommand *dxCommand,const DXSwapChain *dxSwapChain) {
+void DXFunctionHelper::SetRenderTargets(const DXCommand* dxCommand,const DXSwapChain* dxSwapChain){
 	D3D12_CPU_DESCRIPTOR_HANDLE rtvHandle = DXHeap::getInstance()->getRtvCpuHandle(dxSwapChain->getCurrentBackBufferIndex());
 	D3D12_CPU_DESCRIPTOR_HANDLE dsvHandle = DXHeap::getInstance()->getDsvCpuHandle(0);
 	dxCommand->getCommandList()->OMSetRenderTargets(1,&rtvHandle,FALSE,&dsvHandle);
 }
 
-void DXFunctionHelper::PreDraw(const DXCommand *command,const WinApp* window,const DXSwapChain *swapChain) {
+void DXFunctionHelper::PreDraw(const DXCommand* command,const WinApp* window,const DXSwapChain* swapChain){
 	///=========================================
 	//	TransitionBarrierの設定
 	///=========================================
-	ID3D12GraphicsCommandList *commandList = command->getCommandList();
-	
-	D3D12_RESOURCE_BARRIER barrier {};
+	ID3D12GraphicsCommandList* commandList = command->getCommandList();
+
+	D3D12_RESOURCE_BARRIER barrier{};
 	barrier = ResourceBarrierManager::Barrier(
 		swapChain->getCurrentBackBuffer(),
 		D3D12_RESOURCE_STATE_RENDER_TARGET
@@ -100,7 +130,7 @@ void DXFunctionHelper::PreDraw(const DXCommand *command,const WinApp* window,con
 	commandList->OMSetRenderTargets(1,&rtvHandle,FALSE,&dsvHandle);
 
 	//ビューポートの設定
-	D3D12_VIEWPORT viewPort {};
+	D3D12_VIEWPORT viewPort{};
 	viewPort.Width = static_cast<float>(window->getWidth());
 	viewPort.Height = static_cast<float>(window->getHeight());
 	viewPort.TopLeftX = 0;
@@ -110,7 +140,7 @@ void DXFunctionHelper::PreDraw(const DXCommand *command,const WinApp* window,con
 
 	commandList->RSSetViewports(1,&viewPort);
 
-	D3D12_RECT scissorRect {};
+	D3D12_RECT scissorRect{};
 	scissorRect.left = 0;
 	scissorRect.right = window->getWidth();
 	scissorRect.top = 0;
@@ -122,12 +152,12 @@ void DXFunctionHelper::PreDraw(const DXCommand *command,const WinApp* window,con
 	commandList->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
 }
 
-void DXFunctionHelper::PostDraw(DXCommand *dxCommand,DXFence *fence,DXSwapChain *swapChain) {
+void DXFunctionHelper::PostDraw(DXCommand* dxCommand,DXFence* fence,DXSwapChain* swapChain){
 	HRESULT hr;
 	///===============================================================
 	///	バリアの更新(描画->表示状態)
 	///===============================================================
-	D3D12_RESOURCE_BARRIER barrier {};
+	D3D12_RESOURCE_BARRIER barrier{};
 	barrier = ResourceBarrierManager::Barrier(
 		swapChain->getCurrentBackBuffer(),
 		D3D12_RESOURCE_STATE_PRESENT
@@ -143,7 +173,7 @@ void DXFunctionHelper::PostDraw(DXCommand *dxCommand,DXFence *fence,DXSwapChain 
 	///===============================================================
 	/// コマンドリストの実行
 	///===============================================================
-	ID3D12CommandList *ppHeaps[] = {dxCommand->getCommandList()};
+	ID3D12CommandList* ppHeaps[] = {dxCommand->getCommandList()};
 	dxCommand->getCommandQueue()->ExecuteCommandLists(1,ppHeaps);
 	///===============================================================
 
--- Code/System/DirectX12/DXFunctionHelper/DXFunctionHelper.h
+++ Code/System/DirectX12/DXFunctionHelper/DXFunctionHelper.h
@@ -14,13 +14,14 @@
 
 class DXFunctionHelper{
 public:
-	static void CreateBufferResource(const DXDevice *dxDevice,Microsoft::WRL::ComPtr<ID3D12Resource> &resource,size_t sizeInBytes);
+	static void CreateBufferResource(const DXDevice* dxDevice,Microsoft::WRL::ComPtr<ID3D12Resource>& resource,size_t sizeInBytes);
+	static Microsoft::WRL::ComPtr<ID3D12Resource> CreateRenderTextureResource(Microsoft::WRL::ComPtr<ID3D12Device> device,uint32_t width,uint32_t height,DXGI_FORMAT format,const Vector4& clearColor);
 
-	static void ClearRenderTarget(const DXCommand *dxCommand,const DXSwapChain *dxSwapChain);
+	static void ClearRenderTarget(const DXCommand* dxCommand,const DXSwapChain* dxSwapChain);
 
-	static void SetViewportsAndScissor(const DXCommand *dxCommand,const WinApp *window);
-	static void SetRenderTargets(const DXCommand *dxCommand,const DXSwapChain *dxSwapChain);
-	static void PreDraw(const DXCommand *dxCommand,const WinApp *window,const DXSwapChain *dxSwapChain);
-	static void PostDraw(DXCommand *dxCommands,DXFence *dxFence,DXSwapChain *dxSwapChain);
+	static void SetViewportsAndScissor(const DXCommand* dxCommand,const WinApp* window);
+	static void SetRenderTargets(const DXCommand* dxCommand,const DXSwapChain* dxSwapChain);
+	static void PreDraw(const DXCommand* dxCommand,const WinApp* window,const DXSwapChain* dxSwapChain);
+	static void PostDraw(DXCommand* dxCommands,DXFence* dxFence,DXSwapChain* dxSwapChain);
 };
 using DXFH = DXFunctionHelper;
