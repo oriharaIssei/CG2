5d0da3ee88c437158698187c986a5edc555306e1
Amaimono <k023g0048@g.neec.ac.jp>
2024-09-27 22:49:00 +0900

    Load & Play

--- Code/System/Audio/Audio.h
+++ Code/System/Audio/Audio.h
@@ -1,4 +1,54 @@
 #pragma once
-class Audio{
+
+#include <xaudio2.h>
+
+#include <memory>
+#include <wrl.h>
+
+#include <string>
+#include <stdint.h>
+
+struct ChunkHeader{
+	char id[4];
+	int32_t size;
+};
+struct RiffHeader{
+	ChunkHeader chunk;
+	char type[4];
+};
+struct FormatChunk{
+	ChunkHeader chunk;
+	WAVEFORMATEX fmt;
 };
 
+struct SoundData{
+	WAVEFORMATEX wfex;
+	BYTE* pBuffer;
+	uint32_t bufferSize;
+};
+
+class Audio:{
+public:
+	static void StaticInit();
+	static void StaticFinalize();
+
+	void Init(const std::string& fileName);
+
+	void Play();
+	void PlayLoop();
+
+	void Pause();
+
+	void Finalize();
+private:
+	SoundData LoadWave(const std::string& fileName);
+	void SoundUnLoad();
+private:
+	static Microsoft::WRL::ComPtr<IXAudio2> xAudio2_;
+	static IXAudio2MasteringVoice* masterVoice_;
+
+	SoundData soundData_;
+	IXAudio2SourceVoice* pSourceVoice_ = nullptr;
+
+	bool isLoop_ = false;
+};
